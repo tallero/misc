98b5707da4f3e3b392d2d3b675372f82fe276639
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2021-09-07 21:13:00 +0200

    soft power button on pmos

--- formats/tsv_tools.py
+++ formats/tsv_tools.py
@@ -5,11 +5,20 @@ from csv import reader as csv_reader
 from pprint import pprint
 
 def tsv_init(filename):
+    """
+    Args:
+        filename (str)
+    """
     tsv_file = open(filename)
     tsv_read = csv_reader(tsv_file, delimiter="\t")
     return tsv_file, tsv_read
 
 def tsv_read_n_lines(filename, n_lines=1):
+    """
+    Args:
+        filename (str)
+        n_lines (int)
+    """
     tsv_file, tsv_read = tsv_init(filename)
     counter = 0
     for row in tsv_read:
@@ -19,6 +28,13 @@ def tsv_read_n_lines(filename, n_lines=1):
         counter += 1
 
 def tsv_parse(filename):
+    """
+    Args:
+        filename (str)
+
+    Returns:
+        a list of dicts
+    """
     tsv_file, tsv_read = tsv_init(filename)
     data = []
     for row in tsv_read:
@@ -30,3 +46,16 @@ def tsv_parse(filename):
             data_item[fields[i]] = value
         data.append(data_item)
     return data
+
+def search_str_value(data, query):
+    """
+    Args:
+        data [dict]
+        query (str)
+    """
+    results = []
+    for item in data:
+        for field in item:
+            if query in item[field]:
+                results.append(item)
+    return results
